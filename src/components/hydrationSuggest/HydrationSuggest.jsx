c0abe4370c694577706fab9b39eaf32885007d09
Serhii Kozhyn <serginho915@gmail.com>
2025-11-12 20:12:11 +0000

    hydrationsuggest in process

--- src/components/hydrationSuggest/HydrationSuggest.jsx
+++ src/components/hydrationSuggest/HydrationSuggest.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import classes from './HydrationSuggest.module.sass'
+import React, { useEffect, useState } from "react";
+import classes from "./HydrationSuggest.module.sass";
 
 export const HydrationSuggest = () => {
+  const suggestion = "System suggests hydration before further navigation.";
+  const [visibleText, setVisibleText] = useState("");
 
-const suggestion = 'System suggests hydration before further navigation.'
-const [visibleText, setVisibleText] = useState('');
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setVisibleText(suggestion.slice(0, i + 1));
+      i++;
+      if (i >= suggestion.length) clearInterval(interval);
+    }, 75);
 
-useEffect(() => {
-    if (visibleText.length < suggestion.length) {
-      const timeout = setTimeout(() => {
-        setVisibleText(suggestion.slice(0, visibleText.length + 1));
-      }, 75);
+    return () => clearInterval(interval);
+  }, []);
 
-      return () => clearTimeout(timeout);
-    }
-  }, [visibleText, suggestion]);
-
-
-return (
-    <span className={classes.blinkText}>
-      <span className={[classes.cursor,classes.suggestText].join(' ')}>|</span>
-      <span className={classes.suggestText} >{visibleText}</span>
-    </span>
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <span className={classes.cursor}>|</span>
+        <span className={classes.text}>{visibleText}</span>
+      </div>
+    </div>
   );
-}
+};
